82d97918131a93099df85fe562d84950372d599a
Sadık Emre <70113249+sadikemreduzgun@users.noreply.github.com>
2023-03-17 15:25:23 +0300

    Create check.py

---
+++ check.py
@@ -0,0 +1,28 @@
+from reach_time import *
+
+
+def check_installed():
+
+    node_query_exist = False
+    libvirt_query_exist = False
+
+    query_node = "node_load1"
+    query_libvirt = "libvirt_domain_block_stats_allocation"
+
+    url_node = f"http://localhost:9090/api/v1/query_range?query={query_node}"
+    query_libvirt = f"http://localhost:9090/api/v1/query_range?query={query_libvirt}"
+
+    try:
+        data_node = rq.get(url_node).json()
+        data_libvirt = rq.get(query_libvirt).json()
+
+        if data_node['status'] == 'succes':
+            node_query_exist = True
+
+        if data_libvirt['status'] == 'success':
+            libvirt_query_exist = True
+
+    except:
+        pass
+
+    return node_query_exist, libvirt_query_exist
