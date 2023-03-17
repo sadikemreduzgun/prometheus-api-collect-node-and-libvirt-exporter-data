862b0f31e226612ceeba81f695c0104d2c49aee4
Sadık Emre <70113249+sadikemreduzgun@users.noreply.github.com>
2023-03-17 15:25:55 +0300

    Update change_csv.py

--- change_csv.py
+++ change_csv.py
@@ -1,21 +1,35 @@
 import pandas as pd
 import requests as rq
 
-df = pd.read_csv("all_query.csv")
+df = pd.read_csv("all_queries.csv")
+nodes = {}
+libvirts = {}
+node_data =[]
+node_name = []
+libv_data = []
+libv_name=[]
+for name, col in df.iterrows():
 
+    query_name = col["query_name"]
+    query = col["query"]
 
-def org_ins(query):
-
+    print(query)
     # a boolean for searching the word
     found = False
     # define an empty string to be used
     check_word = ""
+
     # if searched word is at the start immediately decide and finish
     # if first 4 letters form node, return instance which is created
     if query[0:4] == "node":
-        # return instance created by return_instance function
-        return "node", "{instance=" + f'{return_instance("node")}' + "}"
 
+        node_data.append(query)
+        node_name.append(query_name)
+    # if first 7 letters form libvirt, return instance which is created
+    if query[0:7] == "libvirt":
+
+        libv_data.append(query)
+        libv_name.append(query_name)
     # if not continue searching
     for char in query:
         # as we know if word is not at the start, it is after a "(" because it must be a query function
@@ -23,61 +37,35 @@ def org_ins(query):
             # search for "(" if found, step on this execution of for loop
             found = True
             continue
-
         # search letters after "(" character
         if found:
             check_word += char
 
         # if "node" is found, return instance as stated
         if check_word == "node":
-            return "node", "{instance=" + f'{return_instance("node")}' + "}"
 
-    # if can't be found return error
-    return -1, -1
+            node_data.append(query)
+            node_name.append(query_name)
+
+        # if "libvirt" is found, return instance as stated
+        if check_word == "libvirt":
+
+            libv_data.append(query)
+            libv_name.append(query_name)
+        # if can't be found return error
 
 
+nodes["query name"] = node_name
+nodes["query"] = node_data
+
+libvirts["query name"] = libv_name
+libvirts["query"] = libv_data
+
+pd.DataFrame(nodes).to_csv("node_queries.csv")
+pd.DataFrame(libvirts).to_csv("libvirt_queries.csv")
+#df_node = pd.DataFrame(nodes)
+#df_libv = pd.DataFrame(libvirts)
+
+#print(df_node, df_libv)
 # a function to return interfaces and jobs, gets functionally-updatet start time
-def return_instance(which=""):
 
-    if which == "node":
-        # assign query
-        query = "node_load1"
-        # load dates and query into URL
-        url = f"http://localhost:9090/api/v1/query?query={query}"
-
-        # get data using requests
-        data = rq.get(url)
-        # turn data into dictionary
-        data = data.json()
-
-        # parse data to get instance
-        result = data['data']['result'][0]['metric']['instance']
-        # return reached instance
-        return f'"{result}"'
-
-    elif which == "libvirt":
-        # assign query
-        query = "libvirt_domain_block_stats_allocation"
-        # load query and time data into URL
-        url = f"http://localhost:9090/api/v1/query_range?query={query}&start={start}&end={end}&step=30s"
-
-        # get data using requests library
-        data = rq.get(url)
-        # turn data into dictionary to be able to pars
-        data = data.json()
-
-        # parse data to get instance
-        result = data['data']['result'][0]['metric']['instance']
-
-        # return data
-        return f'"{result}"'
-
-    # if something goes wrong return error
-    else:
-        return -1
-
-
-for name, col in df.iterrows():
-
-    query_name = col["query_name"]
-    query = col["query"]
